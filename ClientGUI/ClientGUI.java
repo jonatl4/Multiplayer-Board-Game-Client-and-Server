4a6a8522d6546ff1ce0867e723d2b427ce921d16
jonatl4 <jonatl4@uci.edu>
2017-03-16 14:10:06 -0700

    updated GUI

--- ClientGUI/ClientGUI.java
+++ ClientGUI/ClientGUI.java
@@ -2,12 +2,17 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -19,15 +24,19 @@ import javax.swing.text.html.HTMLDocument.Iterator;
 
 import javafx.util.Pair;
 
-public class ClientGUI extends JFrame implements ActionListener, Runnable {
+public class ClientGUI extends Client implements ActionListener, Runnable {
 	
-	private HashMap<Pair<Integer,Integer>, String> grid = new HashMap<Pair<Integer,Integer>, String>();
+	private HashMap<Pair<Integer,Integer>, Piece> grid = new HashMap<Pair<Integer,Integer>, Piece>();
 	private Container contentPane = this.getContentPane();
 	private JPanel gui = new JPanel();
+	private JButton[][] buttons = new JButton[8][8];
 	private final JLabel message = new JLabel("TEST TEXT");
 	
+	private ImageIcon tictactoeX = new ImageIcon("images/x.png");
+	private ImageIcon tictactoeO = new ImageIcon("images/o.png");
+	
 	public ClientGUI(String host, int port) {
-		populateHashMap();
+		chooseGame();
 		initializeGUI();
 	}
 	
@@ -46,16 +55,21 @@ public class ClientGUI extends JFrame implements ActionListener, Runnable {
 		
 		// Board 
 		contentPane.add(gui, BorderLayout.CENTER);
-		gui.setLayout(new GridLayout(0, 8));
+		if (currGame.getGameType().equals("TICTACTOE")) {
+			gui.setLayout(new GridLayout(0, 3));
+		} else {
+			gui.setLayout(new GridLayout(0, 8));
+		}
 		
 		// Buttons
 		int i;
 		int j;
-		JButton[][] buttons = new JButton[8][8];
 		
-		for (Map.Entry<Pair<Integer, Integer>, String> entry : grid.entrySet()) {
-			i = entry.getKey().getKey();
-			j = entry.getKey().getValue();
+		grid = currGame.getCurrentGameBoardState();
+		
+		for (Map.Entry<Pair<Integer, Integer>, Piece> entry : grid.entrySet()) {
+			i = entry.getKey().getKey(); // x coordinate
+			j = entry.getKey().getValue(); // y coordinate
 			buttons[i][j] = new JButton("");
 			buttons[i][j].addActionListener(this);
 			buttons[i][j].setActionCommand("" + i + "" + j);
@@ -71,54 +85,70 @@ public class ClientGUI extends JFrame implements ActionListener, Runnable {
 		contentPane.add(botPanel, BorderLayout.SOUTH);
 	}
 	
-	public void populateHashMap() {
-		for (int i=0; i<8; i++)
-		{
-			for (int j=0; j<8; j++)
-			{
-				Pair<Integer,Integer> pair = new Pair<Integer, Integer>(i, j);
-				grid.put(pair, "EMPTY");
+	public ImageIcon scaleImg(ImageIcon icon, JButton button) {
+		Image img = icon.getImage();
+		Image simg = img.getScaledInstance( button.getWidth(), button.getHeight(), java.awt.Image.SCALE_SMOOTH);
+		icon = new ImageIcon(simg);
+		return icon;
+	}
+	
+	public void actionPerformed(ActionEvent ae) {
+		if(currGame != null) {
+			String command = ae.getActionCommand();
+			int row = Integer.parseInt("" + command.charAt(0));
+			int col = Integer.parseInt("" + command.charAt(1));
+			Dimension dim = new Dimension(row,col);
+			NetworkProtocol move = new NetworkProtocol(NetworkProtocol.ProtocolType.MAKEMOVE, dim);
+			
+			try {
+				output.writeObject(move);
+				System.out.println("Move sent.");
+			} catch (IOException ioe) {
+				JOptionPane.showMessageDialog(this, "\nConnection to Server Lost - Closing");
+				System.exit(0);
 			}
 		}
 	}
 	
-	public void actionPerformed(ActionEvent ae)
-	{
-		String command = ae.getActionCommand();
-		int row = Integer.parseInt("" + command.charAt(0));
-		int col = Integer.parseInt("" + command.charAt(1));
-		Dimension dim = new Dimension(row,col);
+	public void updateBoard(GameBoard update) {
+		int i;
+		int j;
+		ImageIcon icon = new ImageIcon();
+		
+		if (currGame.getGameType().equals("TICTACTOE")) {
+			currGame = update;
+			grid = currGame.getCurrentGameBoardState();
+			
+			for (Map.Entry<Pair<Integer, Integer>, Piece> entry : grid.entrySet()) {
+				i = entry.getKey().getKey(); // x coordinate
+				j = entry.getKey().getValue(); // y coordinate
+				
+				Piece piece = currGame.getPiece(entry.getKey());
+				if (piece.getName().equals("X")) {
+					icon = scaleImg(tictactoeX, buttons[i][j]);
+					buttons[i][j].setIcon(icon);
+				} else if (piece.getName().equals("O")) {
+					icon = scaleImg(tictactoeO, buttons[i][j]);
+					buttons[i][j].setIcon(icon);
+				}
+			}
+		}
 	}
-	
-	public GameBoard chooseGame() {
-		GameBoard board = null;
+	public void chooseGame() {
 		Object[] games = {"Tic Tac Toe", "Checkers", "Othello"};
 		int choice = JOptionPane.showOptionDialog(null, "Choose a game:", 
 				"Game Selection", JOptionPane.DEFAULT_OPTION,	
 				JOptionPane.WARNING_MESSAGE, null, games, games[0]);
-		if(choice == 0) {
-			board = new TicTacToe();
+		if (choice == 0) {
+			currGame = new TicTacToe();
+		} else if (choice == 1) {
+			// board = new Checkers();
+		} else if (choice == 2) {
+			// board = new Othello();
 		}
-		
-		return board;
+
 	}
 	
-	public static void main(String[] args) {
-		ObjectOutputStream oos = null;
-		String host = "localhost";
-		int port = 8000;
-		
-		Runnable r = new Runnable() {
-			
-			@Override
-			public void run() {
-				ClientGUI gui = new ClientGUI(host, port);
-			}
-		};
-		SwingUtilities.invokeLater(r);
-		
-	}
-
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
---
+++ ClientGUI/ServerConnection.java
@@ -0,0 +1,228 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+import javafx.util.Pair;
+
+//import NetworkProtocol.ProtocolType;
+
+public class ServerConnection extends Thread{
+	
+	Socket socket;
+	int clientId;
+	boolean running = true;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private ClientLobby lobby;
+	private NetworkProtocol outgoingData = null;
+	private NetworkProtocol clientInput = null;
+	private String gameType;
+	private int matchId;
+	private int tempMatchCount = 0;//For testing purposes
+	
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	private User user;
+	
+	public ServerConnection(Socket socket, int clientId, ClientLobby lobby){
+		this.matchId = 0;
+		this.socket = socket;
+		this.clientId = clientId;
+		this.lobby = lobby;
+		try{
+			OutputStream o = socket.getOutputStream();
+			output = new ObjectOutputStream(o);
+			InputStream i = socket.getInputStream();
+			input = new ObjectInputStream(i);
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public String getGameType(){
+		return gameType;
+	}
+	
+	public void run(){
+		
+		while(running){
+			try {
+				clientInput = (NetworkProtocol)input.readObject();
+				
+				if(clientInput.getDataType() == NetworkProtocol.ProtocolType.TESTCLIENT){
+					//System.out.println("Validating Account for "+String.valueOf(clientId));
+					outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.TESTSERVER);
+					sendPacket(outgoingData);				
+				}
+				else if(clientInput.getDataType() == NetworkProtocol.ProtocolType.STARTGAME){
+					this.gameType = ((GameBoard) clientInput.getData()).getGameType();
+					System.out.println(this.gameType);
+					if(matchClients(clientId)){
+						GameBoard gb1 = ((GameBoard) clientInput.getData()).clone();
+						System.out.println(gb1);
+						gb1.setTurn(true);
+						Pair<ServerConnection, ServerConnection> activeGame = lobby.getActiveGame(matchId);
+						activeGame.getKey().getUser().setUserToken(1);
+						activeGame.getValue().getUser().setUserToken(2);
+						outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.MAKEMOVE, gb1, activeGame.getKey().getUser());
+//						System.out.println(activeGame.getKey().getMatchId());
+//						System.out.println(activeGame.getValue().getMatchId());
+						activeGame.getKey().sendPacket(outgoingData);
+						GameBoard gb2 = ((GameBoard) clientInput.getData()).clone();
+						gb2.setTurn(false);
+						outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.WAIT, gb2, activeGame.getValue().getUser());
+						activeGame.getValue().sendPacket(outgoingData);
+					}
+				}else if(clientInput.getDataType() == NetworkProtocol.ProtocolType.CLIENTMOVE){
+					Pair<ServerConnection, ServerConnection> activeGame = lobby.getActiveGame(matchId);
+					GameBoard gb1 = ((GameBoard) clientInput.getData()).clone();
+					if(gb1.hasWinner() || gb1.noLegitMoves()){
+						outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.GAMEOVER, gb1);
+						activeGame.getKey().sendPacket(outgoingData);
+						activeGame.getValue().sendPacket(outgoingData);
+					}else{
+						if(gb1.getTurn()){
+							gb1.setTurn(!gb1.getTurn());
+							if(this.getClientId() == activeGame.getKey().getClientId()){
+								outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.WAIT, gb1);
+								activeGame.getKey().sendPacket(outgoingData);
+								GameBoard gb2 = ((GameBoard) clientInput.getData()).clone();
+								gb2.setTurn(!gb1.getTurn());
+								outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.MAKEMOVE, gb2);
+								activeGame.getValue().sendPacket(outgoingData);
+							}else{
+								outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.WAIT, gb1);
+								activeGame.getValue().sendPacket(outgoingData);
+								GameBoard gb2 = ((GameBoard) clientInput.getData()).clone();
+								gb2.setTurn(!gb1.getTurn());
+								outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.MAKEMOVE, gb2);
+								activeGame.getKey().sendPacket(outgoingData);
+							}
+						}
+					}
+				}else if(clientInput.getDataType() == NetworkProtocol.ProtocolType.WAIT){}
+				else if(clientInput.getDataType() == NetworkProtocol.ProtocolType.ACCOUNT)
+				{
+					user = (User)clientInput.getData();
+					if(User.validate(user))
+					{
+						user = User.retrieve(user.getUserName());
+						outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.ACCOUNTVALID);
+						sendPacket(outgoingData);
+						System.out.println("User valid: Account"); // valid user
+					}
+					else					
+					{
+						outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.ACCOUNTINVALID);
+						sendPacket(outgoingData);
+						System.out.println("User invalid: Account"); // invalid user or password
+					}					
+				}
+				else if(clientInput.getDataType() == NetworkProtocol.ProtocolType.NEWACCOUNT)
+				{
+					user = (User)clientInput.getData();
+					
+					if(User.exists(user)) // if account already exist
+					{
+						outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.ACCOUNTINVALID);
+						sendPacket(outgoingData);	
+						System.out.println("User invalid - already taken: NewAccount : " + user.getUserName());
+					}
+					else
+					{
+						User.addAccount(user); // create new account
+						User.saveAccounts();
+						outgoingData = new NetworkProtocol(NetworkProtocol.ProtocolType.ACCOUNTVALID);
+						sendPacket(outgoingData);
+						System.out.println("User added : NewAccount :" + user.getUserName());
+					}
+				}
+				
+			} catch (ClassNotFoundException e) {
+				running = false;
+				System.out.println("Something went wrong");
+				e.printStackTrace();
+			} catch (IOException e) {
+				running = false;
+				System.out.println("Problem with grabbing input");
+			}
+			
+		}
+	}
+	
+	private void sendPacket(NetworkProtocol packet)
+	{
+		try
+		{
+			output.writeObject(packet);
+			output.flush();
+		}
+		catch(IOException ioe)
+		{
+			System.out.println("MESSAGE SEND ABORTED DUE TO SERVER DISCONNECT");
+			
+		}
+	}
+	
+	public boolean matchClients(int clientId){
+		int matchedWaitingClientId = 0;
+		boolean foundGame = false;
+		
+		if(lobby.getClientThreads().size() > 1){
+			for (Map.Entry<Integer, ServerConnection> clientMap : lobby.getClientThreads().entrySet()){
+				if(clientMap.getKey() != clientId){
+					if(((GameBoard) clientInput.getData()).getGameType().equals(clientMap.getValue().getGameType())){
+						matchId = (int)(Math.random()*100);
+						while(!lobby.gameExists(matchId)){
+							if(!lobby.gameExists(matchId)){
+								lobby.setActiveThread(matchId, new Pair<ServerConnection, ServerConnection>(this, clientMap.getValue()));
+								matchedWaitingClientId = clientMap.getValue().getClientId();
+								foundGame = true;
+								clientMap.getValue().setMatchId(matchId);
+								break;
+							}else{
+								System.out.println(matchId);
+								matchId++;
+							}
+						}
+					}else{
+						System.out.println("this is false");
+					}
+				}
+			}
+			if(foundGame){
+				lobby.getClientThreads().remove(clientId);
+				lobby.getClientThreads().remove(matchedWaitingClientId);
+			}
+		}
+		return foundGame;
+	}
+	
+	public int getClientId(){
+		return clientId;
+	}
+	
+	public void setMatchId(int matchId){
+		this.matchId = matchId;
+	}
+	
+	public int getMatchId(){
+		return matchId;
+	}
+	
+
+	
+	
+}
